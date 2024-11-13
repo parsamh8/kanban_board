dcb48363ee73830945793f5010ade291063d1c33
Parsa Mh <157930237+parsamh8@users.noreply.github.com>
2024-11-12 22:00:44 -0500

    login auth complete

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -3,28 +3,42 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    return jwtDecode(this.getToken());
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return token;
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+
+    const decodedToken = jwtDecode<JwtPayload>(token);  // Decode the token
+    const expirationTime = decodedToken.exp || 0;  // Get expiration time
+    const currentTime = Date.now() / 1000;  // Get the current time in milisecond
+    return expirationTime < currentTime;
   }
 
   getToken(): string {
     // TODO: return the token
+    const loggedUser = localStorage.getItem('id_token') || '';
+    return loggedUser;
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
     // TODO: redirect to the home page
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
   }
 
   logout() {
     // TODO: remove the token from localStorage
     // TODO: redirect to the login page
+    localStorage.removeItem('id_token');
+    window.location.assign('/');
   }
 }
 
--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -7,4 +7,22 @@ interface JwtPayload {
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+    const secretKey = process.env.JWT_SECRET_KEY || '';
+
+    jwt.verify(token, secretKey, (err, decodedToken) => {
+      if (err) {
+        return res.sendStatus(403); // Send forbidden status if the token is invalid
+      }
+      // Attach the user information (decodedToken) to the request object
+      // Cast decodedToken as JwtPayload to ensure correct type
+      req.user = decodedToken as JwtPayload;
+
+      return next(); // Call the next middleware function
+    });
+  } else {
+    res.sendStatus(401); // Send unauthorized status if no authorization header is present
+  }
 };
