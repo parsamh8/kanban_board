8c747964d64b14f36cdb011ea97b87cdb24f09fe
Parsa Mh <157930237+parsamh8@users.noreply.github.com>
2024-11-13 23:09:41 -0500

    updatevite.config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,21 +1,8 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/auth': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false
-      },
-    },
-  },
+  envDir: './environment',
+  plugins: [react()],
 });
